5d9dc8b75bcce84c6a9a5ccadd36772200f6d948
Sebastian Degerman <sebbed89@gmail.com>
2025-09-02 11:48:16 +0200

    added another line of text to show versioncontrol

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Bye, World!");
+Console.WriteLine("Wow, another line?!");
